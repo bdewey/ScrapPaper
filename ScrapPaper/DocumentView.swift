4e41f6a434e06a71d53b1e7b4e792fa19ca5be55
Brian Dewey <bdewey@gmail.com>
2020-05-07 06:35:34 -0700

    Initial Commit

---
+++ ScrapPaper/DocumentView.swift
@@ -0,0 +1,21 @@
+// Copyright © 2020 Brian's Brain. All rights reserved.
+
+import SwiftUI
+
+struct DocumentView: View {
+    var document: UIDocument
+    var dismiss: () -> Void
+
+    var body: some View {
+        VStack {
+            HStack {
+                Text("File Name")
+                    .foregroundColor(.secondary)
+
+                Text(document.fileURL.lastPathComponent)
+            }
+
+            Button("Done", action: dismiss)
+        }
+    }
+}
